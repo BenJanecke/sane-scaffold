320f1cace18ae444d0c2abf24054684adffe02a0
Ben Janecke <benjanecke@gmail.com>
2014-04-21 16:56:43 +0200

    Move directory from catchypress to this repo For history https://github.com/BenJanecke/catchy-press/commits/master/lib/generators/filesystem.js https://github.com/BenJanecke/catchy-press/blob/master/test/lib/generators/filesystem-test.js

---
+++ test/lib/filesystem-test.js
@@ -0,0 +1,99 @@
+var Filesystem = require('../../lib/filesystem')
+  , fs = require('fs')
+  , rmdir = require('rmdir');
+
+describe('Filesystem', function () {
+  var filesystem
+    , fixutres;
+
+  before(function (done) {
+    fixtures = __dirname + '/fixtures/filesystem-generator';
+    filesystem = new Filesystem(fixtures);
+    fs.mkdir(fixtures, done);
+  });
+
+  after(function (done) {
+    rmdir(fixtures, done);
+  });
+
+  describe('#directory', function () {
+    it('expects a js directory to exist for "js"', function (done) {
+      filesystem
+      .directory('js', function () {
+        expect(fixtures + '/js').to.be.a.directory();
+        done();
+      });
+    });
+    describe('#done', function () {
+      it('expects a assets directory to exist for "assets"', function (done) {
+        filesystem
+        .directory('assets')
+        .done(function () {
+          expect(fixtures + '/assets').to.be.a.directory();
+          done();
+        });
+      });
+    });
+    describe('#file', function () {
+      it('expects /massets/file.moo to exist for "file.moo"', function (done) {
+        filesystem
+        .directory('massets', function (dir) {
+          dir.file('file.moo', '', function () {
+            expect(fixtures + '/massets/file.moo').to.be.a.file();
+            done();
+          });
+        });
+      });
+    });
+    describe('#directory', function () {
+      it('expects /supremo/directory to exist for "directory"', function (done) {
+        filesystem
+        .directory('supremo', function (dir) {
+          dir.directory('directory', function () {
+            expect(fixtures + '/supremo/directory').to.be.a.directory();
+            done();
+          });
+        });
+      });
+      it('expects /bwaaap/directory/directoryception to exist for "directoryception"', function (done) {
+        filesystem
+        .directory('bwaaap')
+        .directory('directory', function (dir) {
+          dir.directory('directoryception', function () {
+            expect(fixtures + '/directory/directoryception').to.be.a.directory();
+            done();
+          });
+        });
+      });
+    });
+  });
+  describe('#file', function () {
+    it('expects a file.txt file to exist for "file.txt"', function (done) {
+      filesystem
+      .file('file.txt', '', function () {
+        expect(fixtures + '/file.txt').to.be.a.file();
+        done();
+      });
+    });
+    describe('#done', function () {
+      it('expects a another-file.txt file to exist for "another-file.txt"', function (done) {
+        filesystem
+        .file('another-file.txt')
+        .done(function () {
+          expect(fixtures + '/another-file.txt').to.be.a.file();
+          done();
+        });
+      });
+    });
+    describe('#file', function () {
+      it('expects bwaaaaaap.fileception to exist for "bwaaaaaap.fileception"', function (done) {
+        filesystem
+        .file('bwaa.aaap')
+        .file('bwaaaaaap.fileception', '', function () {
+          expect(fixtures + '/bwaaaaaap.fileception').to.be.a.file();
+          done();
+        });
+      });
+    });
+  });
+});
