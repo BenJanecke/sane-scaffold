d0beea0d76afb6b9fa9394c6657c71df0fed572a
Ben Janecke <benjanecke@gmail.com>
2014-04-21 16:57:31 +0200

    Init mocha

---
+++ test/test-helper.js
@@ -0,0 +1,14 @@
+global.chai = require('chai');
+global.expect = chai.expect;
+global.sinon = require('sinon');
+
+function setup () {
+  setupChai();
+};
+
+function setupChai() {
+  chai.use(require('sinon-chai'));
+  chai.use(require('chai-fs'));
+}
+
+module.exports = setup();
